a259d21e8f73ca5a49ac07ed687ecfb72bd39f84
Olga Scrivner <obscrivn@indiana.edu>
2020-10-23 12:25:17 -0400

    zoom level

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ GexfJS.setParams({
         /*
             Default state of the "use lens" button. Set to null to disable button.
         */
-    zoomLevel : 0,
+    zoomLevel : -0.5,
         /*
             Default zoom level. At zoom = 0, the graph should fill a 800x700px zone
          */
